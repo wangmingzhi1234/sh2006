128a2f9d1ec405ad57c44f04a046ada63a313e43
User <2912099865@qq.com>
2021-01-22 14:40:16 +0800

    两次项目

--- src/com/bjpowernoed/test.java
+++ src/com/bjpowernoed/test.java
@@ -1,4 +1,7 @@
 package com.bjpowernoed;
 
 public class test {
+    public static void main(String[] args) {
+        System.out.println("请回答（1+1=？）");
+    }
 }
---
+++ src/com/bjpowernoed/test02.java
@@ -0,0 +1,9 @@
+package com.bjpowernoed;
+
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println(
+                "老铁6660"
+        );
+    }
+}
